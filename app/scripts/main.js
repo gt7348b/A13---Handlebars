520af3fe3673826a3e50db53f50b0dc749273542
John Crocker <johnniecrocker@yahoo.com>
2014-10-28 12:26:19 -0400

    added tempaltes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,6 +2,17 @@ var songs = {
   music: 'Lost',
   composer: 'Michael Buble'
 },
+
+
+var playlist = {
+  songs : [
+    { song: 'Lost', artist: 'Michael Buble' },
+    { song: 'Chow Down on Chik-fil-A', artist: 'Willam'},
+    { song: 'Ligaya', artist: 'Gouryella'},
+    { song: 'Piano Concerto #2', artist: 'Saint-Saens'},
+    { song: 'Empty Streets', artist: 'Tiesto'}
+  ]
+};
     source = $('#music').html(),
     template = Handlebars.compile(source);
 
