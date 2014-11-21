05e857301185b1be750a0afd077ae44cd948bd7a
John Crocker <johnniecrocker@yahoo.com>
2014-11-20 19:35:35 -0500

    gulped

---
+++ dist/scripts/main.js
@@ -0,0 +1,37 @@
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['music'] = template({"1":function(depth0,helpers,partials,data) {
+  var helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
+  return "    <li>"
+    + escapeExpression(((helper = (helper = helpers.song || (depth0 != null ? depth0.song : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"song","hash":{},"data":data}) : helper)))
+    + " by "
+    + escapeExpression(((helper = (helper = helpers.artist || (depth0 != null ? depth0.artist : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"artist","hash":{},"data":data}) : helper)))
+    + "</li>\n";
+},"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+  var stack1, buffer = "\n\n<ul>\n";
+  stack1 = helpers.each.call(depth0, (depth0 != null ? depth0.music : depth0), {"name":"each","hash":{},"fn":this.program(1, data),"inverse":this.noop,"data":data});
+  if (stack1 != null) { buffer += stack1; }
+  return buffer + "</ul>\n";
+},"useData":true});
+})();
+var songs = {
+  music: 'Lost',
+  composer: 'Michael Buble'
+};
+
+
+var playlist = {
+  songs : [
+    { song: 'Lost', artist: 'Michael Buble' },
+    { song: 'Chow Down on Chik-fil-A', artist: 'Willam'},
+    { song: 'Ligaya', artist: 'Gouryella'},
+    { song: 'Piano Concerto #2', artist: 'Saint-Saens'},
+    { song: 'Empty Streets', artist: 'Tiesto'}
+  ]
+};
+    var source = $('#music').html();
+    var template = Handlebars.compile(source);
+    console.log(template);
+    console.log(playlist);
+
+    $('.list').append(template(playlist));
